b66a275c88ab0e9e3566fb66537cf5249844b0cf
Ne02ptzero <louis@ne02ptzero.me>
2016-04-11 21:37:27 +0200

    Fix(Server): Fix header & tabulation

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require('env2')('config.env');
 var request = require('request'),
-express = require('express'),
-compression = require('compression'),
-Twitter = require('twitter'),
-cors = require('cors');
+	express = require('express'),
+	compression = require('compression'),
+	Twitter = require('twitter'),
+	cors = require('cors');
 
 var app = express();                 // define our app using express
 
@@ -21,92 +21,76 @@ var router = express.Router();              // get an instance of the express Ro
 app.use(compression());
 
 // Varnish header
-app.all('/*', function(req, res, next) {
+app.all('*', function(req, res, next) {
 	res.header('Cache-Control', 'public, max-age=120');
 	next();
 });
 
 router.get('/bambuser', function(req, res) {
-  var options = {
-    uri: 'http://api.bambuser.com/broadcast.json',
-    qs: {
-      api_key: process.env.BAMBUSER_APIKEY,
-      tag: 'NuitDeboutLive',
-      type: 'live'
-    }
-  }
+	var options = {
+		uri: 'http://api.bambuser.com/broadcast.json',
+		qs: {
+			api_key: process.env.BAMBUSER_APIKEY,
+			tag: 'NuitDeboutLive',
+			type: 'live'
+		}
+	}
 
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-      body = JSON.parse(body);
-
-      if (body.result && !body.result.length)
-      {
-
-        delete options.qs.type;
-
-        request(options, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-
-            res.json(body);
-
-          }
-        });
-
-      }
-      else
-      {
-        res.json(body);
-      }
-
-    }
-  });
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			if (body.result && !body.result.length) {
+				delete options.qs.type;
+				request(options, function (error, response, body) {
+					if (!error && response.statusCode == 200) {
+						res.json(body);
+					}
+				});
+			} else {
+			res.json(body);
+			}
+		}
+	});
 });
 
 
 router.get('/facebook', function (req, res) {
+	var options = {
+		uri: 'https://graph.facebook.com/1707017119576184/posts',
+		qs: {
+			access_token: process.env.FACEBOOK_ACCESS_TOKEN,
+			fields: 'message,caption,full_picture,link'
+		}
+	}
 
-  var options = {
-    uri: 'https://graph.facebook.com/1707017119576184/posts',
-    qs: {
-      access_token: process.env.FACEBOOK_ACCESS_TOKEN,
-      fields: 'message,caption,full_picture,link'
-    }
-  }
-
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-
-      body = JSON.parse(body);
-      res.setHeader('Content-Type', 'application/json');
-      res.json(body);
-
-    }
-  });
-
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			res.setHeader('Content-Type', 'application/json');
+			res.json(body);
+		}
+	});
 });
 
 router.get('/twitter', function (req, res) {
+	var client = new Twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
 
-  var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
-
-  var params = {screen_name: 'nuitdebout'};
-  client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-      res.json(tweets);
-    }
-  });
+	var params = {screen_name: 'nuitdebout'};
+	client.get('statuses/user_timeline', params, function (error, tweets, response) {
+		if (!error) {
+			res.json(tweets);
+		}
+	});
 });
 
 // Enable CORS access
 var corsOptions = {
-  origin: process.env.CORS_ALLOWED_URL
+	origin: process.env.CORS_ALLOWED_URL
 };
 
 app.use(cors(corsOptions));
